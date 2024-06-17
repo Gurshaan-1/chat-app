fa0fcc2c1c847b8a1a11b321a5523ac9fdc62ade
Gurshaanpreet Singh <gurshaan1507@gmail.com>
2024-06-17 19:14:31 +0530

    Deployment1

--- frontend/context/socketContext.jsx
+++ frontend/context/socketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-gg9u.onrender.com", {
+      const socket = io("https://chat-app-1-yjk3.onrender.com", {
         query: {
           userId: authUser._id,
         },
